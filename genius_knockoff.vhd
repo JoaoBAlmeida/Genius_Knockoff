6d34b71e60e12a9fe3b658508b9b68cb324ccc04
João Pedro Barbosa <jpbarbosinha@gmail.com>
2021-11-30 22:34:54 -0300

    Wrong detection fixed

--- genius_knockoff.vhd
+++ genius_knockoff.vhd
@@ -8,8 +8,7 @@ entity genius_knockoff is
 	port(
 		clk, rst : in std_logic;
 		btn0, btn1, btn2, btn3 : in std_logic; -- Game buttons
-		led0, led1, led2, led3 : out std_logic; -- Visual representation for players
-		arrayed : out ledarray
+		led0, led1, led2, led3 : out std_logic -- Visual representation for players
 	);
 end entity;
 
@@ -19,6 +18,7 @@ architecture game of genius_knockoff is
 	signal gameleds: ledarray := (1, 3, 4, 2, 3, 1, 4, 2, 3, 1, 4, 3, 2, 4, 1, 4, 3, 2, 2, 4, 3, 3, 4, 1, 2);
 	signal difficulty : state := easy;
 	signal usermode : bool := false;
+	signal wrong_active : bool := false;
 	signal led_on : bool := false;
 	
 	--Dependent on difficulty
@@ -65,15 +65,36 @@ architecture game of genius_knockoff is
 		
 	end procedure;
 	
-	--Procedure to set wrong choice in game
-	procedure wrong_choice is
+	-- Turn on all leds
+	procedure all_leds_on is
 	begin
-		
+	
 		led0 <= '1';
 		led1 <= '1';
 		led2 <= '1';
 		led3 <= '1';
 		led_on <= true;
+		
+	end procedure;
+	
+	-- Turn off all leds
+	procedure all_leds_off is
+	begin
+		
+		led0 <= '0';
+		led1 <= '0';
+		led2 <= '0';
+		led3 <= '0';
+		led_on <= false;
+		
+	end procedure;
+	
+	--Procedure to set wrong choice in game
+	procedure wrong_choice is
+	begin
+		
+		all_leds_on;
+		wrong_active <= true;
 		
 	end procedure;
 		
@@ -85,11 +106,24 @@ begin
 	begin
 	
 		if rst = '1' then
+			all_leds_off;
 			difficulty <= easy;
+			usermode <= false;
+			wrong_active <= false;
+			led_on <= false;
 		elsif clk'event and clk = '1' then
 		
 			-- Section for Led presentation
 			if usermode = false then
+			
+				-- Clear user inputs
+				if led_on = true then -- Make sure no Led came on from User Input
+					all_leds_off;
+				end if;
+				if wrong_active = true then
+					wrong_active <= false;
+				end if;
+				
 				size := array_range;
 				if i < size then -- Show the led while variable for control lower then size
 					case gameleds(i) is -- Run the array reading expected led
@@ -122,30 +156,26 @@ begin
 					i := i + 1; -- To keep loop running
 				else -- Reset variable for control, and set gamemode to user input
 				
-					led0 <= '0';
-					led1 <= '0';
-					led2 <= '0';
-					led3 <= '0';
+					all_leds_off;
 					
 					i := 0;
 					usermode <= true;
 				end if;
 				
-			--Section User Input
 			-- Section for User input
 			else
 				size := array_range;
 				
 				if led_on = true then -- Check to keep leds off after each round
-					led0 <= '0';
-					led1 <= '0';
-					led2 <= '0';
-					led3 <= '0';
-					led_on <= false;
+					all_leds_off;
 				end if;
 						
 				if i < size then -- While still not on size, play the game
-					if btn0 = '1' then
+					if wrong_active = true then
+						
+						i := i + 1; -- To keep loop running
+						
+					elsif btn0 = '1' then
 					
 						if array_value(i) = 1 then -- Correct Button Pressed
 							led0 <= '1';
@@ -194,7 +224,13 @@ begin
 				else -- Reset variable for control and gamemode
 					i := 0;
 					usermode <= false; -- Reset game to LedMode
-					change_difficulty; -- Increment Difficulty
+					
+					if wrong_active = false then -- Next Round Difficulty dependent on wrong pressed
+						change_difficulty; -- Increment Difficulty
+					else
+						difficulty <= easy;
+					end if;
+					
 				end if;
 			end if;
 		end if;
